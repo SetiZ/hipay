34d3e2e7646584f18738970c5d60f1e2e7167faa
Mehdi Fekih <mehdi@adyoulike.com>
2016-11-01 18:10:18 +0100

    add elements

--- lib.js
+++ lib.js
@@ -21,11 +21,9 @@ HiPayRequestor.sendRequest = function(params) {
   invisible.id = "form_iframe"
   // invisible.style = "width:0;height:0;border:0px solid #fff;"
 
-
   // var form = "<form id ='form' method='POST' action='"+url+"'><input name='redirect' value='"+data["redirect"]+"'><input name='data' value='"+data["data"]+"'></form>"
 
   // invisible.src = 'data:text/html;charset=utf-8,' + encodeURI(form);
-
 
   invisible.src = "form.html"
 
@@ -33,35 +31,28 @@ HiPayRequestor.sendRequest = function(params) {
 
   // $('#form_iframe')[0].contentDocument.write(form)
 
-  $('#form_iframe').on('load', function() {
-      // $('#form_iframe').contents().find('form')[0].submit();
+  $('#form_iframe').on("load", function() {
+    console.log("la")
+      $('#form_iframe').contents().find('form')[0].submit(function(event) {
+        event.preventDefault();
+        console.log("ici")
+        var req = $.ajax({
+          method: "POST",
+          url: "http://test-javascript.000webhostapp.com/signParams.php",
+          data: $('#form_iframe').contents().find('form').find("input").serialize(),
+        })
+        req.done(function (response) {
+          console.log("here", response)
+          result.resolve(response)
+        })
+        req.fail(function() {
+          result.reject();
+          console.log("la")
+        })
 
-        $('#form_iframe').contents().find('form')[0].submit(function() {
-          $.ajax({
-            url: "http://test-javascript.000webhostapp.com/signParams.php",
-            type: POST,
-            data : $('#form_iframe').contents().find('form').find("input").serialize(), // $('#form_iframe').contents().find('form')[0].serialize(),
-            success: function(response) {
-              console.log(response)
-              console.log($('#form_iframe').contents().find('body').html())
-              result.resolve()
-            },
-            error: function() {
-              result.reject()
-            }
-          });
-          return result.promise()
       });
-
   })
 
-  // return result.promise()
+  return result.promise()
 
-  // var post = $.ajax({
-  //   type: "POST",
-  //   url: url,
-  //   data: data
-  // })
-  //
-  // return post
 }
