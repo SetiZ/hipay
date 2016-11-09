e8b48826518ee1d0fcc92acd93e259a8472ae939
Mehdi Fekih <mehdi@adyoulike.com>
2016-11-09 16:45:13 +0100

    form outside of iframe

--- lib.js
+++ lib.js
@@ -21,19 +21,41 @@ HiPayRequestor.sendRequest = function(params) {
   invisible.id = "form_iframe"
   // invisible.style = "width:0;height:0;border:0px solid #fff;"
 
-  invisible.src = "form.html"
+  // invisible.src = "form.html"
   document.body.appendChild(invisible)
+
+  var form = document.createElement("form");
+  form.action = url;
+  form.method = "POST"
+  form.target = invisible.id;
+  var redirectInput = document.createElement("input");
+  redirectInput.name = "redirect"
+  redirectInput.value = data["redirect"]
+  var dataInput = document.createElement("input");
+  dataInput.name = "data"
+  dataInput.value = data["data"]
+
+  form.appendChild(redirectInput)
+  form.appendChild(dataInput)
+  // form.style.display = "none";
+document.body.appendChild(form);
+
+// form.submit();
+
+// document.body.removeChild(form);
 
   $('#form_iframe').on("load", function() {
     console.log("la")
     console.log($('#form_iframe'))
     console.log($('#form_iframe')[0].src)
-      $('#form_iframe').contents().find('form')[0].submit(function(event) {
-        event.preventDefault()
-        console.log("ici")
-        console.log($('#form_iframe'))
-        console.log($('#form_iframe').location)
-      });
+      // $('#form_iframe').contents().find('form').submit(function() {
+      //
+      //   console.log("ici")
+      //   console.log($('#form_iframe'))
+      //   console.log($('#form_iframe').location)
+      //
+      // });
+      return result.promise()
   })
 
   return result.promise()
